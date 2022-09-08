09d9324ca1774c1d858eb3027be5deac2022b41d
Arunachalam N <arunachalam@Arunachalams-MacBook-Pro.local>
2022-09-08 10:42:18 +0530

    chore:deposit form

--- src/pages/Home/Depoist.tsx
+++ src/pages/Home/Depoist.tsx
@@ -1,23 +1,77 @@
+import React, { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { useWeb3React } from "@web3-react/core";
+
 import { Button } from "components";
-import React from "react";
+import { getPlanInfo } from "utils/methods";
+import { formatNumber } from "helpers/utilities";
+import { TransactionContext } from "store/context/TransactionContext";
+
+type IPlan = {
+  planId: number;
+  time: number;
+  percent: number;
+};
 
 const Depoist = () => {
+  const { account, chainId, library } = useWeb3React();
+  const [plans, setPlans] = useState<IPlan[]>([]);
+  const [selectedPlan, setSelectedPlan] = useState<IPlan | undefined>();
+  const [depositAmount, setDepositAmount] = useState("");
+  const { setTransaction } = useContext(TransactionContext);
+
+  const handleGetplansData = useCallback(async () => {
+    if (!account || !chainId) return;
+    const data = await getPlanInfo(account, library?.provider, chainId);
+    setPlans(data);
+    setSelectedPlan(data[0]);
+  }, [account, chainId, library]);
+
+  useEffect(() => {
+    handleGetplansData();
+  }, [handleGetplansData]);
+
+  const totalReturns = useMemo(() => {
+    if (!depositAmount) return 1;
+    if (!selectedPlan) return 1;
+
+    return (selectedPlan.percent / 100) * Number(depositAmount) * selectedPlan.time;
+  }, [depositAmount, selectedPlan]);
+
+  const handleInvest = async () => {
+    try {
+      if (!account || !chainId || !selectedPlan) return;
+      setTransaction({ loading: true, status: "pending" });
+      setTransaction({ loading: true, status: "success" });
+      const { invest } = await import("utils/methods");
+      await invest(account, library?.provider, chainId, "", selectedPlan.planId, depositAmount);
+    } catch (error) {
+      setTransaction({ loading: true, status: "error" });
+    }
+  };
+
   return (
     <div className="deposit">
       <section className="deposit_controls">
         <div>
           <p>Deposit Period (days)</p>
-          <select>
-            <option value="10">10</option>
-            <option value="10">10</option>
-            <option value="10">10</option>
-            <option value="10">10</option>
+          <select
+            onChange={(e) => setSelectedPlan(plans.find((f) => f.time === Number(e.target.value)))}
+          >
+            {plans.map((plan, index) => (
+              <option key={index.toString()} value={plan.time}>
+                {plan.time}
+              </option>
+            ))}
           </select>
         </div>
         <div>
           <p>Deposit Amount</p>
           <div>
-            <input type="number" />
+            <input
+              type="number"
+              value={depositAmount}
+              onChange={(e) => setDepositAmount(e.target.value)}
+            />
             <button>Max</button>
           </div>
         </div>
@@ -25,18 +79,20 @@ const Depoist = () => {
       <section className="deposit_stats">
         <div>
           <p>Daily ROI</p>
-          <span>4</span>
+          <span>{!selectedPlan ? 0 : selectedPlan.percent}</span>
         </div>
         <div>
           <p>Total Profit</p>
-          <span>25%</span>
+          <span>{!selectedPlan ? "0%" : `${selectedPlan.time * selectedPlan.percent}%`}</span>
         </div>
         <div>
           <p>In 50 days you will earn</p>
-          <span>2.00000 BNB</span>
+          <span>{formatNumber(totalReturns, 4)} BNB</span>
         </div>
       </section>
-      <Button variant="secondary">Invest</Button>
+      <Button variant="secondary" onClick={() => handleInvest()}>
+        Invest
+      </Button>
     </div>
   );
 };
--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -2,34 +2,30 @@ import { ethers, utils } from "ethers";
 import tokenABi from "./abi/bnbstake.json";
 import { STAKE_ADDRESS } from "./address";
 
-export const loadContract = (
-  address: string,
-  provider,
-  chainid: string | number
-) => {
+export const loadContract = (address: string, provider, chainid: string | number) => {
   const etherProvider = new ethers.providers.Web3Provider(provider);
   const signer = etherProvider.getSigner(address);
-  const escrowContract = new ethers.Contract(
-    STAKE_ADDRESS[chainid],
-    tokenABi,
-    signer
-  );
+  const escrowContract = new ethers.Contract(STAKE_ADDRESS[chainid], tokenABi, signer);
   return escrowContract;
 };
 
-export const getMinimumDepositAmount = async (address, provider, chainid) => {
+export const getMinimumDepositAmount = async (
+  address: string,
+  provider,
+  chainid: number | string
+) => {
   const stake = loadContract(address, provider, chainid);
   const minimumDepositAmt = await stake.INVEST_MIN_AMOUNT();
-  return minimumDepositAmt.toString();
+  return Number(minimumDepositAmt.toString());
 };
 
 export const invest = async (
-  address,
+  address: string,
   provider,
-  chainid,
-  referrer,
-  plan,
-  amount
+  chainid: number | string,
+  referrer: string,
+  plan: number,
+  amount: string
 ) => {
   const stake = loadContract(address, provider, chainid);
   const amtinWei = utils.parseEther(amount.toString()).toString();
@@ -39,44 +35,68 @@ export const invest = async (
   await tx.wait();
 };
 
-export const withdraw = async (address, provider, chainid) => {
+export const withdraw = async (address: string, provider, chainid: number | string) => {
   const stake = loadContract(address, provider, chainid);
   const tx = await stake.withdraw();
   await tx.wait();
 };
 
-export const getUserReferrer = async (address, provider, chainid) => {
+export const getUserReferrer = async (address: string, provider, chainid: number | string) => {
   const stake = loadContract(address, provider, chainid);
   const referrer = await stake.getUserReferrer();
   return referrer;
 };
 
-export const getUserTotalReferrals = async (address, provider, chainid) => {
+export const getUserTotalReferrals = async (
+  address: string,
+  provider,
+  chainid: number | string
+) => {
   const stake = loadContract(address, provider, chainid);
   const referrer = await stake.getUserTotalReferrals();
   return referrer.toString();
 };
 
-export const getUserTotalWithdrawn = async (address, provider, chainid) => {
+export const getUserTotalWithdrawn = async (
+  address: string,
+  provider,
+  chainid: number | string
+) => {
   const stake = loadContract(address, provider, chainid);
   const referrer = await stake.getUserTotalWithdrawn();
   return referrer.toString();
 };
 
-export const getUserCheckpoint = async (address, provider, chainid) => {
+export const getUserCheckpoint = async (address: string, provider, chainid: number | string) => {
   const stake = loadContract(address, provider, chainid);
   const referrer = await stake.getUserCheckpoint();
   return referrer.toString();
 };
 
-export const getUserAvailable = async (address, provider, chainid) => {
+export const getUserAvailable = async (address: string, provider, chainid: number | string) => {
   const stake = loadContract(address, provider, chainid);
   const referrer = await stake.getUserAvailable();
   return referrer.toString();
 };
 
-export const getUserTotalDeposits = async (address, provider, chainid) => {
+export const getUserTotalDeposits = async (address: string, provider, chainid: number | string) => {
   const stake = loadContract(address, provider, chainid);
   const referrer = await stake.getUserTotalDeposits();
   return referrer.toString();
+};
+
+export const getPlanInfo = async (address: string, provider, chainid: number | string) => {
+  const stake = loadContract(address, provider, chainid);
+  const data = await Promise.all(
+    Array.from({ length: 5 }).map(async (_, i) => {
+      const referrer = await stake.getPlanInfo(i);
+      return {
+        planId: i,
+        time: Number(referrer[0].toString()),
+        percent: Number(referrer[1].toString()) / 10,
+      };
+    })
+  );
+
+  return data;
 };
